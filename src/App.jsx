f4a78dd5b7d10772acb354008a33c14a2921b038
muhamasaidnurmahatov-jpg <muhamasaidnurmahatov@gmail.com>
2025-12-16 10:51:56 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ function App() {
   const incrementCount = countStore(s => s.incrementCount)
   const decrementCount = countStore(s => s.decrementCount)
   const resetCount = countStore(s => s.resetCount)
-  const plusCount = countStore(s => s.plusCount)
-  const minusCount = countStore(s => s.minusCount)
 
   return (
     <>
@@ -13,8 +11,6 @@ function App() {
       <button onClick={() => incrementCount()}>+</button>
       <button onClick={() => decrementCount()}>-</button>
       <button onClick={() => resetCount()}>Reset</button>
-      <button onClick={() => plusCount()}>+5</button>
-      <button onClick={() => minusCount()}>-5</button>
     </>
   )
 }
